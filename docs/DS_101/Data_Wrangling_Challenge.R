321c3fa16644a42828caa261e64e91175920a31e
Moamen Elbahy <46838788+MoamenElbahy@users.noreply.github.com>
2020-12-01 14:55:02 +0100

    2

---
+++ docs/DS_101/Data_Wrangling_Challenge.R
@@ -0,0 +1,12 @@
+################################ Business Data Science Basics ################################
+# Chapter 3 Data Wrangling: Challenge.
+
+install.packages("data.table")
+library(vroom)
+
+library(data.table)
+url <- "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
+covid_data_dt <- fread(url)
+
+class(covid_data_dt)
+##  "data.table" "data.frame"
